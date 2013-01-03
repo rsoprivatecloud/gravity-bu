d31e68178726656c0abc9d608265d3c5f33bc689
David Wittman <david.wittman@rackspace.com>
2013-01-03 11:06:57 -0600

    Use attributes for cronjob

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -1,7 +1,16 @@
-default["gravitybackup"]["chefvm"] = "chef-server"
-default["gravitybackup"]["chefip"] = "169.254.123.2"
-default["gravitybackup"]["vmdiskloc"] = "/opt/rpcs/chef-server.qcow2"
-default["gravitybackup"]["vmxmlloc"] = "/opt/rpcs/chef-server.xml"
-default["gravitybackup"]["backupdir"] = "/var/chef-backup"
-default["gravitybackup"]["corenum"] = "4"
-default["gravitybackup"]["vcorenum"] = "2"
+default["gravitybackup"] = {
+  "chefvm" => "chef-server",
+  "chefip" => "169.254.123.2",
+  "vmdiskloc" => "/opt/rpcs/chef-server.qcow2",
+  "vmxmlloc" => "/opt/rpcs/chef-server.xml",
+  "backupdir" => "/var/chef-backup",
+  "corenum" => "4",
+  "vcorenum" => "2",
+  "cron" => {
+    "minute" => "0",
+    "hour" => "4",
+    "day" => "*",
+    "month" => "*",
+    "weekday" => "0"
+  }
+}
